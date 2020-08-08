2e885cd2d115fe3b675a85822d33307817ea0f2a
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-08-08 10:09:24 +0100

    initial commit: starting with a hello world console application in dotnet 2.2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace packages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        } // static void Main(string[] args)
+    } // class Program
+} // namespace packages
